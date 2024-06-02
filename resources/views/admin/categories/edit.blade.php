18d8d90b8dfcb2603704f45cfc32afc7e84ec2f5
David García <ccristhiangarcia@gmail.com>
2024-06-02 13:59:07 -0500

    Add edit and delete functions to category module

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin.app')
+
+@section('content')
+    <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+        <main>
+            <div class="px-4 pt-6">
+                <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <!-- Card header -->
+                    <div class="items-center justify-between lg:flex">
+                        <div class="mb-4 lg:mb-0">
+                            <h3 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Edit category: {{ $category->name }}</h3>
+                        </div>
+                        <div class="items-center sm:flex">
+                            <div class="flex items-center">
+                                <a href="{{ route('admin.categories.index') }}" class="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Categories
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col mt-6">
+                        <form action="{{ route('admin.categories.update', $category) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                <div class="sm:col-span-2">
+                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Name</label>
+                                    <input type="text" name="name" id="name" value="{{ old('name', $category->name) }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type tag name">
+                                    @error('name')
+                                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                            <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                                Edit category
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+@endsection
